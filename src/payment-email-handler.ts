aa4f483f7cdfc21db4fd3d5e946dab23af2e3427
Michael Meuli <michael.meuli@gmail.com>
2021-06-11 15:50:33 +0200

    add swissqrbill, first version

--- src/payment-email-handler.ts
+++ src/payment-email-handler.ts
@@ -12,6 +12,12 @@ import {
 import { EmailEventHandler } from '@vendure/email-plugin';
 import { EmailEventListener } from '@vendure/email-plugin';
 
+const SwissQRBill = require("swissqrbill");
+
+
+
+
+
 
 export const sendInvoiceHandler = new EmailEventListener('send-invoice')
     .on(PaymentStateTransitionEvent)
@@ -19,28 +25,217 @@ export const sendInvoiceHandler = new EmailEventListener('send-invoice')
         event =>
             event.toState === 'Authorized' && event.payment.method === 'swiss-qr-invoice'  // this.code in payment-method-handler.ts via super(config) (config: PaymentMethodConfigOptions<T>) (interface PaymentMethodConfigOptions<T extends ConfigArgs> extends ConfigurableOperationDefOptions<T>) 
     )
-    .loadData(async context => {
-        const shippingMethods: ShippingMethod[] = [];  
+    .setAttachments(async (event) => {
 
-        for (const line of context.event.order.shippingLines || []) {
-            let shippingMethod: ShippingMethod | undefined;
-            if (!line.shippingMethod && line.shippingMethodId) {
-                shippingMethod = await context.injector
-                    .get(TransactionalConnection)
-                    .getRepository(ShippingMethod)
-                    .findOne(line.shippingMethodId);
-            } else if (line.shippingMethod) {
-                shippingMethod = line.shippingMethod;
+        const data = {
+            currency: "CHF",
+            amount: event.order.totalWithTax,
+            additionalInformation: event.order.code,
+            creditor: {
+              name: "SwissQRBill",
+              address: "Bahnhofstrasse 7",
+              zip: 1234,
+              city: "Musterstadt",
+              account: "CH4431999123000889012",
+              country: "CH"
+            },
+            debtor: {
+              name: event.order.customer?.firstName.concat(" ", event.order.customer?.lastName),
+              address: event.order.customer?.addresses[0].streetLine1,
+              zip: event.order.customer?.addresses[0].postalCode,
+              city: event.order.customer?.addresses[0].city,
+              country: event.order.customer?.addresses[0].country
             }
-            if (shippingMethod) {
-                shippingMethods.push(shippingMethod);
-            }
+          };
+
+        const pdf = new SwissQRBill.PDF(data, "complete-qr-bill.pdf", { "autoGenerate": false, "size": "A4" });
+
+
+//-- Add creditor address
+
+pdf.fontSize(12);
+pdf.fillColor("black");
+pdf.font("Helvetica");
+pdf.text(data.creditor.name + "\n" + data.creditor.address + "\n" + data.creditor.zip + " " + data.creditor.city, SwissQRBill.utils.mmToPoints(20), SwissQRBill.utils.mmToPoints(35), {
+  width: SwissQRBill.utils.mmToPoints(100),
+  height: SwissQRBill.utils.mmToPoints(50),
+  align: "left"
+});
+
+
+//-- Add debtor address
+
+pdf.fontSize(12);
+pdf.font("Helvetica");
+pdf.text(data.debtor.name + "\n" + data.debtor.address + "\n" + data.debtor.zip + " " + data.debtor.city, SwissQRBill.utils.mmToPoints(130), SwissQRBill.utils.mmToPoints(60), {
+  width: SwissQRBill.utils.mmToPoints(70),
+  height: SwissQRBill.utils.mmToPoints(50),
+  align: "left"
+});
+
+
+//-- Add title
+
+pdf.fontSize(14);
+pdf.font("Helvetica-Bold");
+pdf.text("Rechnung Nr. 1071672", SwissQRBill.utils.mmToPoints(20), SwissQRBill.utils.mmToPoints(100), {
+  width: SwissQRBill.utils.mmToPoints(170),
+  align: "left"
+});
+
+const date = new Date();
+
+pdf.fontSize(11);
+pdf.font("Helvetica");
+pdf.text("Musterstadt " + date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear(), {
+  width: SwissQRBill.utils.mmToPoints(170),
+  align: "right"
+});
+
+
+//-- Add table
+
+const table = {
+  width: SwissQRBill.utils.mmToPoints(170),
+  rows: [
+    {
+      height: 30,
+      fillColor: "#ECF0F1",
+      columns: [
+        {
+          text: "Position",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "Anzahl",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "Bezeichnung"
+        }, {
+          text: "Total",
+          width: SwissQRBill.utils.mmToPoints(30)
         }
+      ]
+    }, {
+      columns: [
+        {
+          text: "1",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "14 Std.",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "Programmierung SwissQRBill"
+        }, {
+          text: "CHF 1'540.00",
+          width: SwissQRBill.utils.mmToPoints(30)
+        }
+      ]
+    }, {
+      columns: [
+        {
+          text: "2",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "8 Std.",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "Dokumentation"
+        }, {
+          text: "CHF 880.00",
+          width: SwissQRBill.utils.mmToPoints(30)
+        }
+      ]
+    }, {
+      height: 40,
+      columns: [
+        {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "Summe",
+          font: "Helvetica-Bold"
+        }, {
+          text: "CHF 2'420.00",
+          font: "Helvetica-Bold",
+          width: SwissQRBill.utils.mmToPoints(30)
+        }
+      ]
+    }, {
+      columns: [
+        {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "MwSt."
+        }, {
+          text: "7.7%",
+          width: SwissQRBill.utils.mmToPoints(30)
+        }
+      ]
+    }, {
+      columns: [
+        {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "MwSt. Betrag"
+        }, {
+          text: "CHF 186.35",
+          width: SwissQRBill.utils.mmToPoints(30)
+        }
+      ]
+    }, {
+      height: 40,
+      columns: [
+        {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "",
+          width: SwissQRBill.utils.mmToPoints(20)
+        }, {
+          text: "Rechnungstotal",
+          font: "Helvetica-Bold"
+        }, {
+          text: "CHF 2'606.35",
+          width: SwissQRBill.utils.mmToPoints(30),
+          font: "Helvetica-Bold"
+        }
+      ]
+    }
+  ]
+};
 
-        return { shippingMethods };
+pdf.addTable(table);
+
+
+//-- Add QR slip
+
+pdf.addQRBill();
+
+
+//-- Finalize the document
+
+pdf.end();
+
+
+        return [
+          {
+            filename: "complete-qr-bill.pdf",
+            path: pdf,
+          },
+        ];
     })
     .setRecipient(event => event.order.customer!.emailAddress)
     .setFrom(`{{ fromAddress }}`)
     .setSubject(`Order confirmation for #{{ order.code }}`)
-    .setTemplateVars(event => ({ order: event.order, shippingMethods: event.data.shippingMethods }))
-    .setMockEvent(mockOrderStateTransitionEvent);
+    .setTemplateVars(event => ({ order: event.order}));
