fb4fbbc5f0a93702565c08f5e81d77f99e216623
Michael Meuli <michael.meuli@gmail.com>
2021-08-09 20:33:27 +0200

    test cors

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -21,6 +21,9 @@ export const config: VendureConfig = {
         port: 3000,
         adminApiPath: 'admin-api',
         shopApiPath: 'shop-api',
+        cors: {
+            "origin": "https://vigorous-swanson-b61fb2.netlify.app/"
+        }
     },
     authOptions: {
         tokenMethod: 'cookie',
